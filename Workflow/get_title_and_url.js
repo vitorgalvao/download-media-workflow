ac10e1ddf3e77cde6edb988467fe4b98fb9f7903
Vítor Galvão <info@vitorgalvao.com>
2023-05-26 20:40:33 +0100

    Add support for Orion and Arc

--- Workflow/get_title_and_url.js
+++ Workflow/get_title_and_url.js
@@ -1,19 +1,19 @@
 #!/usr/bin/osascript -l JavaScript
 
-const frontmost_app_name = Application('System Events').applicationProcesses.where({ frontmost: true }).name()[0]
-const frontmost_app = Application(frontmost_app_name)
+const frontmostAppName = Application("System Events").applicationProcesses.where({ frontmost: true }).name()[0]
+const frontmostApp = Application(frontmostAppName)
 
-const chromium_variants = ['Google Chrome', 'Chromium', 'Opera', 'Vivaldi', 'Brave Browser', 'Microsoft Edge']
-const webkit_variants = ['Safari', 'Webkit']
+const chromiumVariants = ["Google Chrome", "Chromium", "Opera", "Vivaldi", "Brave Browser", "Microsoft Edge", "Arc"]
+const webkitVariants = ["Safari", "Webkit", "Orion"]
 
-if (chromium_variants.some(app_name => frontmost_app_name.startsWith(app_name))) {
-  var current_tab_title = frontmost_app.windows[0].activeTab.name()
-  var current_tab_url = frontmost_app.windows[0].activeTab.url()
-} else if (webkit_variants.some(app_name => frontmost_app_name.startsWith(app_name))) {
-  var current_tab_title = frontmost_app.windows[0].currentTab.name()
-  var current_tab_url = frontmost_app.windows[0].currentTab.url()
+if (chromiumVariants.some(appName => frontmostAppName.startsWith(appName))) {
+  var currentTabTitle = frontmostApp.windows[0].activeTab.name()
+  var currentTabURL = frontmostApp.windows[0].activeTab.url()
+} else if (webkitVariants.some(appName => frontmostAppName.startsWith(appName))) {
+  var currentTabTitle = frontmostApp.windows[0].currentTab.name()
+  var currentTabURL = frontmostApp.windows[0].currentTab.url()
 } else {
-  throw new Error('You need a supported browser as your frontmost app')
+  throw new Error("You need a supported browser as your frontmost app")
 }
 
-current_tab_url + '\n' + current_tab_title
+currentTabURL + "\n" + currentTabTitle
